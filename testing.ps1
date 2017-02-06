b93fe1a2859c67dd57f37203990ebacb52b3dbb9
argonaut4479 <jlcrawfish@gmail.com>
2017-02-06 11:23:18 -0600

    Update testing.ps1

--- testing.ps1
+++ testing.ps1
@@ -1,3 +1,3 @@
  Add-Type -AssemblyName system.speech
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$speak.Speak("Fuzzy wuzzy was a bear.  Fuzzy wuzzy had no hair.  Fuzzy wuzzy wasn't fuzzy was he?") 
+$speak.Speak("When the moon hits your eye like a big pizza pie, that's amore.") 
