9ec4c309b53c901dd61752ddd67ab1bc86da724f
argonaut4479 <jlcrawfish@gmail.com>
2017-02-06 11:22:08 -0600

    Update testing.ps1

--- testing.ps1
+++ testing.ps1
@@ -1,1 +1,3 @@
-Get-Service
+ Add-Type -AssemblyName system.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.Speak("Fuzzy wuzzy was a bear.  Fuzzy wuzzy had no hair.  Fuzzy wuzzy wasn't fuzzy was he?") 
