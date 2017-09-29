75c2e2d6b673e651eab108dd94804438a15716db
argonaut4479 <jlcrawfish@gmail.com>
2017-09-29 11:43:55 -0500

    Update testing.ps1

--- testing.ps1
+++ testing.ps1
@@ -3,6 +3,6 @@ $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
 $speak.Volume=100
 $i = 0
 do {
-  $speak.Speak("I love Trump!")
+  $speak.Speak("Texas BBQ is the best!")
   $i++
 } while ($i -le 20)
